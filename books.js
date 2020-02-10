901a9332051a762a3c95223809aad28d216df170
ProfStefanoDUrso <prof.stefano.durso@gmail.com>
2020-02-10 21:22:43 +0100

    list & book

---
+++ books.js
@@ -0,0 +1,5 @@
+exports.books = [
+    {"author":"stefano","title":"tecn and prog","code":"68","available":"1"},
+    {"author":"sara","title":"computer science","code":"130","available":"1"},
+    {"author":"tiziana","title":"system","code":"170","available":"1"}
+];
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const express = require('express');
 const app = express();
+const books = require('./books')
 
 app.use(bodyParser.json());
 app.listen(process.env.PORT || 3000,() => {
@@ -12,6 +13,33 @@ app.get('/', function (req, res) {
   res.send('Hello NAO!');
 });
 
-app.get('*',(req,res) => {
-    res.sendFile(path.join(__dirname,'./index.html'));
+app.get('/list', function (req, res) {
+    res.contentType('application/json');
+    myJSONstring = JSON.stringify(books);
+    res.send(myJSONstring);
 });
+
+app.get('/book/:id', function (req, res) {
+    id=req.params['id'];
+    res.contentType('application/json');
+    myJSONstring = JSON.stringify(books);
+    var myJSONstringP = JSON.parse(myJSONstring);
+    var _books=myJSONstringP.books.filter(x=>x.code == id && x.available == 1);
+    res.send(_books);
+});
+
+app.get('/booki/:id', function (req, res) {
+    id=req.params['id'];
+    myJSONstring = JSON.stringify(books);
+    var myJSONstringP = JSON.parse(myJSONstring);
+    var _books=myJSONstringP.books.filter(x=>x.code == id && x.available == 1);
+    if (_books.length>0)
+        res.send("1");
+    else
+        res.send("0");
+});
+
+
+// app.get('*',(req,res) => {
+//     res.sendFile(path.join(__dirname,'./index.html'));
+// });
